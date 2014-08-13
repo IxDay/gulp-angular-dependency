130136f07d65a02682a9fe05a8d7f12d226dd3ba
Maxime Vidori <maxime.vidori@gmail.com>
2014-08-13 23:16:04 +0200

    Debuggable tests

--- test/main.js
+++ test/main.js
@@ -1,10 +1,22 @@
-var assert = require("assert")
+var assert = require('assert');
+var File   = require('gulp-util').File;
+var Buffer = require('buffer').Buffer;
+var prefix = require('../');
 
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    })
-  })
-})
+
+describe('prefix testing', function () {
+	var input = 'foo';
+
+  it('should prefix', function (done) {
+  	var stream = prefix('test');
+  	stream.on('data', function (newFile) {
+  		assert.equal(newFile.contents.toString(), 'testfoo');
+  		done();
+  	});
+
+  	stream.write(new File({
+  		contents: new Buffer(input)
+  	}));
+  	stream.end();
+  });
+});
