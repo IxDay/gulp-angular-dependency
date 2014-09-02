ce3296f123803677baecf8d17616e03cb9297956
Maxime Vidori <maxime.vidori@gmail.com>
2014-09-02 13:34:17 +0200

    Add tests, correct some behaviour

--- index.js
+++ index.js
@@ -12,8 +12,14 @@ const PLUGIN_NAME = 'gulp-angular-dependency';
 // plugin level function (dealing with files)
 function gulpAngularDependency (modules) {
 
+  function addToStream (stream, module) {
+    stream.push(files[module.defined]);
+    _.each(module.contents, function (path) {
+      stream.push(files[path]);
+    });
+  }
+
   var files = {};
-  modules = modules || [];
   if (typeof modules === 'string') { modules = [modules]; }
 
   // creating a stream through which each file will pass
@@ -31,15 +37,17 @@ function gulpAngularDependency (modules) {
     var topology = angularModulesFactory.getAngularModules();
     var that = this;
 
-    _.each(modules, function (module) {
-      _.each(topology.resolve(module, true), function (module) {
-        that.push(files[module.defined]);
-
-        _.each(module.contents, function (path) {
-          that.push(files[path]);
+    if (modules) {
+      _.each(modules, function (module) {
+        _.each(topology.resolve(module, true), function (module) {
+          addToStream(that, module);
         });
       });
-    });
+    } else {
+      _.each(topology.modules, function (module) {
+        addToStream(that, module);
+      });
+    }
     cb();
   });
 }
--- test/main.js
+++ test/main.js
@@ -1,26 +1,61 @@
-var assert  = require('assert');
-var vfs     = require('vinyl-fs');
-var through = require('through2');
-var prefix  = require('../');
-var path    = require('path');
+var assert             = require('assert');
+var vfs                = require('vinyl-fs');
+var through            = require('through2');
+var angularDependency  = require('../');
+var path               = require('path');
 
 
 describe('gulp-angular-dependency', function () {
+  var files;
+
+  beforeEach(function () {
+    files = [];
+  });
 
   it('should filter files which contains the needed module and its ' +
     'dependencies', function (done) {
-      var files = [];
-      vfs.src('test/test_case/*')
-        .pipe(prefix('module1'))
-        .pipe(through.obj(function (chunk, enc, cb) {
-          files.push(path.relative(__dirname, chunk.path));
-          cb();
-        }, function () {
-          assert.deepEqual(files, [
-            'test_case/file_0_2.js',
-            'test_case/file_0_1.js',
-            'test_case/file_0_3.js']);
-          done();
-        }));
+    vfs.src('test/test_case/test_case_1/*')
+      .pipe(angularDependency('module1'))
+      .pipe(through.obj(function (chunk, enc, cb) {
+        files.push(path.relative(__dirname, chunk.path));
+        cb();
+      }, function () {
+        assert.deepEqual(files, [
+          'test_case/test_case_1/file_0_2.js',
+          'test_case/test_case_1/file_0_1.js',
+          'test_case/test_case_1/file_0_3.js']);
+        done();
+      }));
+  });
+
+  it('should be possible to require multiple modules', function (done) {
+    vfs.src('test/test_case/test_case_2/*')
+      .pipe(angularDependency(['module1', 'module2']))
+      .pipe(through.obj(function (chunk, enc, cb) {
+        files.push(path.relative(__dirname, chunk.path));
+        cb();
+      }, function () {
+        assert.deepEqual(files, [
+          'test_case/test_case_2/file_0_1.js',
+          'test_case/test_case_2/file_0_3.js',
+          'test_case/test_case_2/file_0_2.js']);
+        done();
+      }));
+  });
+
+  it('if no module is required it pass all files containing angular code',
+    function (done) {
+      vfs.src('test/test_case/test_case_3/*')
+      .pipe(angularDependency())
+      .pipe(through.obj(function (chunk, enc, cb) {
+        files.push(path.relative(__dirname, chunk.path));
+        cb();
+      }, function () {
+        assert.deepEqual(files, [
+          'test_case/test_case_3/file_0_1.js',
+          'test_case/test_case_3/file_0_3.js',
+          'test_case/test_case_3/file_0_2.js']);
+        done();
+      }));
   });
 });
